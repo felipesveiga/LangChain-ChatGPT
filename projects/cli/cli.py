3ee99a3ebed737710f919f7d8e0e2a4428d9d309
felipesveiga <felipesveiga@gmail.com>
2024-01-05 14:55:04 -0300

    Aula 15 (Extender a CLI conforme o diagrama de 0:55)

--- projects/cli/cli.py
+++ projects/cli/cli.py
@@ -1,5 +1,29 @@
+from langchain.prompts import PromptTemplate
 from langchain.llms.openai import OpenAI
+from langchain.chains import LLMChain
+from utils._parser import _args
 
-llm = OpenAI() # Classe do modelo.
-result = llm('Tell me about LangChain') # Resultado da consulta.
-print(result)
+def main():
+    '''
+        Executa a CLI, fazendo uma requisição à OpenAI e retornando seu output.
+    '''
+    args = _args(['--language', '--task'])
+
+    # Prompt Template a ser encaminhado à OpenAI
+    prompt = PromptTemplate(template='Write a short {language} function that will {task}',
+                        input_variables=['language', 'task']
+                        )
+    # Classe do LLM.
+    llm = OpenAI() 
+
+    # Instanciando a Chain da CLI e invocando-a com os argumentos do usuário.
+    chain = LLMChain(llm=llm, prompt=prompt)
+    result = chain(inputs={
+        'language':args.language,
+        'task':args.task
+    })
+
+    print(result['text'])
+
+if __name__ == '__main__':
+    main()
