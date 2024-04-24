52cf24aff3f9d490e4cb5e1961aaab2ed7499342
felipesveiga <felipesveiga@gmail.com>
2024-04-24 09:01:33 -0300

    Aula 95

--- projects/pdf/app/chat/vectorstore/pinecone.py
+++ projects/pdf/app/chat/vectorstore/pinecone.py
@@ -2,6 +2,8 @@ from pinecone import init
 from langchain.vectorstores.pinecone import Pinecone
 from langchain.vectorstores.pinecone import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_core.vectorstores import VectorStoreRetriever
+from app.chat.models import ChatArgs
 from os import getenv
 
 init(api_key=getenv('PINECONE_API_KEY'),
@@ -11,4 +13,22 @@ vector_store = Pinecone.from_existing_index(
     getenv('PINECONE_INDEX_NAME'), OpenAIEmbeddings()
 )
 
-vector_store.as_retriever()
+def build_retriever(chat_args:ChatArgs)->VectorStoreRetriever:
+    '''
+        Builds a Vector DB retriever parameterized with the provided 
+        chat's metadata.
+
+        Parameter
+        ---------
+        `chat_args`: `ChatArgs`
+            A Pydantic object holding the chat's metadata.
+
+        Returns
+        -------
+        A `langchain_core.vectorstores.VectorStoreRetriever` parameterized
+        for only retrieving the desired chat's chunks. 
+    '''
+    search_kwargs = {'filter':{'pdf_id':chat_args.pdf_id}}
+    return vector_store.as_retriever(
+        search_kwargs=search_kwargs
+    )
