4c7db345bf398f33f939218dcc00b949bbb268ef
felipesveiga <felipesveiga@gmail.com>
2024-05-17 09:17:47 -0300

    Aula 114 (2:00); Iniciar edição do callback

--- projects/pdf/app/chat/chat.py
+++ projects/pdf/app/chat/chat.py
@@ -1,4 +1,5 @@
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 from app.chat.llms.chatopenai import build_llm
 from app.chat.vectorstore.pinecone import build_retriever
 from app.chat.memories.sql_memory import build_memory
@@ -17,10 +18,12 @@ def build_chat(chat_args: ChatArgs):
         chain = build_chat(chat_args)
     """
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     retriever = build_retriever(chat_args)
     memory = build_memory(chat_args)
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
         memory=memory,
-        retriever=retriever
+        retriever=retriever,
+        condense_question_llm=condense_question_llm
     )
